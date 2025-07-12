dd242d7fe767c21fd24c382a9ad8cfb4f82270fa
Akarshit Kaushik <akarshit0206@gmail.com>
2025-07-12 19:24:32 +0000

    solve: Leetcode 410 problem binary Search Approach

---
+++ problems/leetcode/Leetcode_410.java
@@ -0,0 +1,31 @@
+// Question Link: https://leetcode.com/problems/split-array-largest-sum/
+
+class Solution {
+  public static int min(int n1, int n2) {
+    if (n1 >= n2)
+      return n1;
+    return n2;
+  }
+
+  public int minEatingSpeed(int[] piles, int h) {
+    int low = 1;
+    int high = piles.length;
+    int res = high;
+    while (low <= high) {
+      int k = low + (high - low) / 2;
+      int hours = 0;
+      for (int ele : piles) {
+        hours += Math.ceil(ele / k);
+      }
+      if (hours <= h) {
+        res = min(res, k);
+        high = k - 1;
+      }
+      if (hours > h)
+        low = k + 1;
+    }
+    return res;
+
+  }
+
+}
