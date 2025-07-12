72f35c3599639b7f946502033870f0e9a227bce5
Akarshit Kaushik <akarshit0206@gmail.com>
2025-07-12 19:44:01 +0000

    fix: errors in leetcode 410

--- problems/leetcode/Leetcode_410.java
+++ problems/leetcode/Leetcode_410.java
@@ -3,19 +3,30 @@
 class Solution {
   public static int min(int n1, int n2) {
     if (n1 >= n2)
-      return n1;
-    return n2;
+      return n2;
+    return n1;
+  }
+
+  public int findMax(int[] arr) {
+    int max = 0;
+    for (int ele : arr) {
+      if (ele > max)
+        max = ele;
+    }
+    return max;
   }
 
   public int minEatingSpeed(int[] piles, int h) {
+
     int low = 1;
-    int high = piles.length;
+    int high = findMax(piles);
     int res = high;
     while (low <= high) {
       int k = low + (high - low) / 2;
       int hours = 0;
       for (int ele : piles) {
-        hours += Math.ceil(ele / k);
+        double hour = (double) ele / k;
+        hours += Math.ceil(hour);
       }
       if (hours <= h) {
         res = min(res, k);
@@ -25,7 +36,6 @@ class Solution {
         low = k + 1;
     }
     return res;
-
   }
 
 }
