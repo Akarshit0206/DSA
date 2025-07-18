7688be4bcc1b2a6fa4da344d7a8913594d5a8998
Akarshit Kaushik <akarshit0206@gmail.com>
2025-07-18 09:13:29 +0000

    optimize: Leetcode 1(Two Sum) hashmap approach

--- problems/leetcode/Leetcode_1.java
+++ problems/leetcode/Leetcode_1.java
@@ -1,17 +1,22 @@
+
 //Question Link: https://leetcode.com/problems/two-sum/description/
+import java.util.HashMap;
 
 class Solution {
   public int[] twoSum(int[] nums, int target) {
-    int[] ans = { 0, 0 };
+
+    HashMap<Integer, Integer> hash = new HashMap<>();
+    int[] res = { 0, 0 };
+    for (int i = 0; i < nums.length; i++)
+      hash.put(nums[i], i);
     for (int i = 0; i < nums.length; i++) {
-      for (int j = i; j < nums.length; j++) {
-        if (nums[i] + nums[j] == target && i != j) {
-          ans[0] = i;
-          ans[1] = j;
-          break;
-        }
+      int secondNumber = target - nums[i];
+      if (hash.get(secondNumber) != null && hash.get(secondNumber) != i) {
+        res[0] = i;
+        res[1] = hash.get(secondNumber);
+        break;
       }
     }
-    return ans;
+    return res;
   }
 }
