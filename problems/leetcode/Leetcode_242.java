1f4707e07d7d3afb9cdf26fa3ffd9757349872a9
Akarshit Kaushik <akarshit0206@gmail.com>
2025-07-22 09:09:23 +0000

    optimize: leetcode 242 (valid anagram) by sorting approach

--- problems/leetcode/Leetcode_242.java
+++ problems/leetcode/Leetcode_242.java
@@ -1,36 +1,17 @@
 //Question Link: https://leetcode.com/problems/valid-anagram/
 
-import java.util.HashMap;
+import java.util.Arrays;
 
 class Solution {
   public boolean isAnagram(String s, String t) {
     if (s.length() != t.length())
       return false;
-    HashMap<Character, Integer> charCount = new HashMap<>();
-    for (int i = 0; i < s.length(); i++) {
-      char characterOfS = s.charAt(i);
-      char characterOfT = t.charAt(i);
-      boolean wasNullS = false;
-      boolean wasNullT = false;
-      if (charCount.get(characterOfS) == null) {
-        charCount.put(characterOfS, 1);
-        wasNullS = true;
-      }
-      if (charCount.get(characterOfT) == null) {
-        charCount.put(characterOfT, -1);
-        wasNullT = true;
-      }
-      if (!wasNullS)
-        charCount.put(characterOfS, charCount.get(characterOfS) + 1);
-      if (!wasNullT)
-        charCount.put(characterOfT, charCount.get(characterOfT) - 1);
-    }
-    System.out.println(charCount);
-    for (int count : charCount.values()) {
-      if (count != 0)
-        return false;
-    }
+    char[] arrayOfS = s.toCharArray();
+    char[] arrayOfT = t.toCharArray();
+    Arrays.sort(arrayOfS);
+    Arrays.sort(arrayOfT);
 
-    return true;
+    return Arrays.equals(arrayOfS, arrayOfT);
+
   }
 }
