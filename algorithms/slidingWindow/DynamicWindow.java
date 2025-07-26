43082480c5cd7bfc3c749e47eee3ab68e7380b85
Akarshit Kaushik <akarshit0206@gmail.com>
2025-07-26 10:03:00 +0000

    optimize: DynamicWindow pattern algorithm

--- algorithms/slidingWindow/DynamicWindow.java
+++ algorithms/slidingWindow/DynamicWindow.java
@@ -2,6 +2,8 @@
 
 //"This is the basic template which other question of this pattern follow"
 
+//*This optimization of while loop to restrict the window's size to go below maxSize works if we have to find the length of longest subArray but if we have to return the subArray itself so we have use the older approach and stick to nested while loop as this can approach incorrect result*
+
 class DynamicWindow {
   public static int longestSubarraySize(int[] arr, int k) {
     int l = 0;
@@ -10,7 +12,7 @@ class DynamicWindow {
     int sum = 0;
     while (r < arr.length) {
       sum += arr[r];
-      while (sum > k) {
+      if (sum > k) {
         sum -= arr[l];
         l++;
       }
