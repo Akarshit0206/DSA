0704b4eb9c02c688e7289a10bd365d6a749f4ce1
Akarshit Kaushik <akarshit0206@gmail.com>
2025-08-04 17:53:38 +0000

    solve: leetcode 74 (Search a 2D Matrix) binary search approach

--- problems/leetcode/Leetcode_74.java
+++ problems/leetcode/Leetcode_74.java
@@ -1,16 +1,24 @@
 //Question Link: https://leetcode.com/problems/search-a-2d-matrix/description/
 
 class Solution {
-  public boolean searchMatrix(int[][] matrix, int target) {
-    int row = 0;
-    int col = matrix[row].length - 1;
-    while (row < matrix.length && col >= 0) {
-      if (matrix[row][col] == target)
+
+  public boolean binarySearch(int[] arr, int target, int low, int high) {
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (arr[mid] == target)
         return true;
-      if (matrix[row][col] > target)
-        col--;
+      if (arr[mid] > target)
+        high = mid - 1;
       else
-        row++;
+        low = mid + 1;
+    }
+    return false;
+  }
+
+  public boolean searchMatrix(int[][] matrix, int target) {
+    for (int i = 0; i < matrix.length; i++) {
+      if (binarySearch(matrix[i], target, 0, matrix[i].length - 1))
+        return true;
     }
     return false;
   }
