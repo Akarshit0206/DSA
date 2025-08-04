aba9cd22eed51cc8bfce5b6be7b8f0045736c011
Akarshit Kaushik <akarshit0206@gmail.com>
2025-08-04 17:45:34 +0000

    solve: Leetcode 74 (Search a 2D Matrix)

---
+++ problems/leetcode/Leetcode_74.java
@@ -0,0 +1,17 @@
+//Question Link: https://leetcode.com/problems/search-a-2d-matrix/description/
+
+class Solution {
+  public boolean searchMatrix(int[][] matrix, int target) {
+    int row = 0;
+    int col = matrix[row].length - 1;
+    while (row < matrix.length && col >= 0) {
+      if (matrix[row][col] == target)
+        return true;
+      if (matrix[row][col] > target)
+        col--;
+      else
+        row++;
+    }
+    return false;
+  }
+}
