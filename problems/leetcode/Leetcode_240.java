39557f0d733021ba90d419bf238991eda37a50d2
Akarshit Kaushik <akarshit0206@gmail.com>
2025-08-05 17:41:36 +0000

    optimize: leetcode 240 (Search a 2D Matrix II)

--- problems/leetcode/Leetcode_240.java
+++ problems/leetcode/Leetcode_240.java
@@ -1,28 +1,14 @@
 //Question Link: https://leetcode.com/problems/search-a-2d-matrix-ii/description/
-
 class Solution {
 
-  public boolean binarySearch(int[] arr, int target, int low, int high) {
-    while (low <= high) {
-      int mid = low + (high - low) / 2;
-      if (arr[mid] == target)
-        return true;
-      if (arr[mid] < target)
-        low = mid + 1;
-      else
-        high = mid - 1;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row =0;
+        int col= matrix[0].length-1;
+        while(row<matrix.length && col>=0){
+            if(matrix[row][col] == target) return true;
+            if(matrix[row][col] > target) col--;
+            else row++;
+        }
+        return false;
     }
-    return false;
-  }
-
-  public boolean searchMatrix(int[][] matrix, int target) {
-    int row = 0;
-    int col = matrix[0].length - 1;
-    while (row < matrix.length) {
-      if (binarySearch(matrix[row], target, 0, col))
-        return true;
-      row++;
-    }
-    return false;
-  }
 }
