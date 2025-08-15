7064e8fca2df42ff7fe310b9553c5ab0c4fab914
Akarshit Kaushik <akarshit0206@gmail.com>
2025-08-15 15:42:07 +0000

    optimize: Leetcode 11 (Container With Most Water)

--- problems/leetcode/Leetcode_11.java
+++ problems/leetcode/Leetcode_11.java
@@ -1,21 +1,15 @@
 //Question Link: https://leetcode.com/problems/container-with-most-water/description/
 
 class Solution {
-  public int maxArea(int[] height) {
-    int left = 0, maxArea = 0;
-    int len = height.length;
-    while (left < len) {
-      int right = left + 1;
-      if (height[left] * (len - left) > maxArea) {
-        while (right < len) {
-          int windowSize = right - left;
-          int area = Math.min(height[left], height[right]) * windowSize;
-          maxArea = Math.max(area, maxArea);
-          right++;
+    public int maxArea(int[] height) {
+        int len = height.length;
+        int left = 0, right=len-1, maxArea = 0;
+        while (left < right) {
+            int area= (right-left) *Math.min(height[left], height[right]);
+            maxArea= Math.max(area, maxArea);
+            if(height[left]<height[right]) left++;
+            else right--;
         }
-      }
-      left++;
+        return maxArea;
     }
-    return maxArea;
-  }
 }
