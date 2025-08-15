6076482a403925baa68b5926be7653f54a3f6cca
Akarshit Kaushik <akarshit0206@gmail.com>
2025-08-15 16:52:30 +0000

    optimize leetcode 121 (Best Time to Buy and Sell Stock)

--- problems/leetcode/Leetcode_121.java
+++ problems/leetcode/Leetcode_121.java
@@ -1,22 +1,17 @@
 //Question Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-
 class Solution {
   public int maxProfit(int[] prices) {
     int len = prices.length;
-    int left = 0, leastBuyValue = Integer.MAX_VALUE, maxProfit = 0;
-    while (left < len - 1) {
-      if (prices[left] < leastBuyValue) {
-        int right = len - 1;
-        while (right > left) {
-          int profit = prices[right] - prices[left];
-          maxProfit = Math.max(profit, maxProfit);
-          right--;
-        }
-        leastBuyValue = prices[left];
+    int left = 0, right = 1, maxProfit = 0;
+    while (right < len) {
+      if (prices[left] > prices[right]) {
+        left++;
+        continue;
       }
-      left++;
+      int profit = prices[right] - prices[left];
+      maxProfit = Math.max(profit, maxProfit);
+      right++;
     }
     return maxProfit;
-
   }
 }
