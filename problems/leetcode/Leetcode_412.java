8b1244501da7f8786da5bb20c84c3bfdced14569
Akarshit Kaushik <akarshit0206@gmail.com>
2025-08-16 19:07:16 +0000

    optimize: Leetcode 412 (Fizz Buzz)

--- problems/leetcode/Leetcode_412.java
+++ problems/leetcode/Leetcode_412.java
@@ -4,19 +4,23 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Solution {
-  public List<String> fizzBuzz(int n) {
-    List<String> res = new ArrayList<>();
-    for (int i = 1; i <= n; i++) {
-      if (i % 3 == 0 && i % 5 == 0) {
-        res.add("FizzBuzz");
-      } else if (i % 3 == 0) {
-        res.add("Fizz");
-      } else if (i % 5 == 0) {
-        res.add("Buzz");
-      } else {
-        res.add(String.format("%d", i));
-      }
+    public List<String> fizzBuzz(int n) {
+        List<String> res= new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            
+            if(i%15==0){
+                res.add("FizzBuzz");
+            }
+            else if(i%3==0){
+                res.add("Fizz");
+            }
+            else if(i%5==0){
+                res.add("Buzz");
+            }
+            else{
+                res.add(String.valueOf(i));
+            }
+        }
+        return res;
     }
-    return res;
-  }
 }
