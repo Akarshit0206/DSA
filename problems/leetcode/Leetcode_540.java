4a8aece039b30e1a81c13f44ade2dae1614a714b
Akarshit Kaushik <akarshit0206@gmail.com>
2025-08-26 19:53:57 +0000

    optimize: leetcode 540 (Single Element in a Sorted Array)

--- problems/leetcode/Leetcode_540.java
+++ problems/leetcode/Leetcode_540.java
@@ -2,17 +2,27 @@
 
 class Solution {
   public int singleNonDuplicate(int[] nums) {
-    int ans = 0;
-    for (int i = 0; i < nums.length; i += 2) {
-      if (i + 1 < nums.length && nums[i + 1] != nums[i]) {
-        ans = nums[i];
-        break;
+    int len = nums.length;
+    if (len == 1)
+      return nums[0];
+    if (nums[0] != nums[1])
+      return nums[0];
+    if (nums[len - 1] != nums[len - 2])
+      return nums[len - 1];
+
+    int low = 1;
+    int high = len - 2;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+        return nums[mid];
       }
-      if (i + 1 >= nums.length) {
-        ans = nums[i];
-        break;
+      if (mid % 2 == 1 && nums[mid - 1] == nums[mid] || mid % 2 == 0 && nums[mid + 1] == nums[mid]) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
       }
     }
-    return ans;
+    return -1;
   }
 }
