863b864b27a3f145cd43540c3e11869027b99cc6
Akarshit Kaushik <akarshit0206@gmail.com>
2025-08-28 09:24:39 +0000

    solve: Leetcode 189 (Rotate Array) bruteforce solution {time-limit exceeds}

---
+++ problems/leetcode/Leetcode_189.java
@@ -0,0 +1,15 @@
+//Question Link: https://leetcode.com/problems/rotate-array/
+
+class Solution {
+  public void rotate(int[] nums, int k) {
+    int n = nums.length;
+    k = k % n;
+    for (int j = 1; j <= k; j++) {
+      int item = nums[n - 1];
+      for (int i = n - 2; i >= 0; i--) {
+        nums[i + 1] = nums[i];
+      }
+      nums[0] = item;
+    }
+  }
+}
