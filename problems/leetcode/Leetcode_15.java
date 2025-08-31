322932111c0a05c95b50bd4d490d21414d956492
Akarshit Kaushik <akarshit0206@gmail.com>
2025-08-31 19:03:34 +0000

    optimize: Leetcode 15 (3Sum) two pointer approach

--- problems/leetcode/Leetcode_15.java
+++ problems/leetcode/Leetcode_15.java
@@ -1,27 +1,37 @@
 //Question Link:https://leetcode.com/problems/3sum/description/
 
-import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 
 class Solution {
   public List<List<Integer>> threeSum(int[] nums) {
-    HashSet<List<Integer>> hashSetOfLists = new HashSet<>();
-    for (int i = 0; i < nums.length; i++) {
-      HashSet<Integer> set = new HashSet<>();
-      for (int j = i + 1; j < nums.length; j++) {
-        int kValue = -(nums[i] + nums[j]);
-        if (set.contains(kValue)) {
-          List<Integer> li = new ArrayList<>(Arrays.asList(nums[i], nums[j], kValue));
-          Collections.sort(li);
-          hashSetOfLists.add(li);
+    int len = nums.length;
+    Arrays.sort(nums);
+    List<List<Integer>> res = new ArrayList<>();
+    for (int i = 0; i < len; i++) {
+      if (i > 0 && nums[i] == nums[i - 1])
+        continue;
+      int j = i + 1;
+      int k = len - 1;
+      while (j < k) {
+        int sum = nums[i] + nums[j] + nums[k];
+        if (sum < 0) {
+          j++;
+        } else if (sum > 0) {
+          k--;
+        } else {
+          List<Integer> li = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k]));
+          res.add(li);
+          j++;
+          k--;
+          while (j < k && nums[j] == nums[j - 1])
+            j++;
+          while (j < k && nums[k] == nums[k + 1])
+            k--;
         }
-        set.add(nums[j]);
       }
     }
-    List<List<Integer>> res = new ArrayList<>(hashSetOfLists);
     return res;
   }
 }
